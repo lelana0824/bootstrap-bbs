4243a5fb42cfc4046dca7066800bc9697915b005
lelana0824 <lelana0824@gmail.com>
2019-04-05 21:30:52 +0900

    생성

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Header />
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+export default {
+  name: 'App',
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Vue.js로 게시판 만들기</b-navbar-brand>
+
+      <b-navbar-toggle target="nav_collapse"/>
+
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item href="#">공지사항</b-nav-item>
+          <b-nav-item href="#">자유게시판</b-nav-item>
+          <b-nav-item href="#">구인구직</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {};
+  }
+};
+</script>
+
