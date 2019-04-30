bc5d2527f1152db77c487f3cea4a8b029dc91524
lelana0824 <lelana0824@gmail.com>
2019-04-30 20:18:14 +0900

    5강 내용

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,51 +1,79 @@
 <template>
   <div>
-    <b-table striped hover :items="items" :fields="fields" @row-clicked="rowClick"></b-table>
+    <b-table 
+      striped hover 
+      :per-page="perPage"
+      :current-page="currentPage" 
+      :items="items" 
+      :fields="fields" 
+      @row-clicked="rowClick"></b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      align="center"
+    ></b-pagination>
     <b-button @click="writeContent">글쓰기</b-button>
   </div>
 </template>
 
 <script>
-import data from '@/data'
+import data from "@/data";
 
 export default {
-  name: 'Board',
+  name: "Board",
   data() {
-    let items = data.Content.sort((a,b) => {return b.content_id - a.content_id})
-items = items.map(contentItem => {return {...contentItem, user_name: data.User.filter(userItem => userItem.user_id === contentItem.user_id)[0].name}})
+    let items = data.Content.sort((a, b) => {
+      return b.content_id - a.content_id;
+    });
+    items = items.map(contentItem => {
+      return {
+        ...contentItem,
+        user_name: data.User.filter(
+          userItem => userItem.user_id === contentItem.user_id
+        )[0].name
+      };
+    });
     return {
+      currentPage: 1,
+      perPage: 10,
       fields: [
         {
-          key: 'content_id',
-          label: '글번호',
+          key: "content_id",
+          label: "글번호"
         },
         {
-          key: 'title',
-          label: '제목',
+          key: "title",
+          label: "제목"
         },
         {
-          key: 'created_at',
-          label: '등록일'
+          key: "created_at",
+          label: "등록일"
         },
         {
-          key: 'user_name',
-          label: '글쓴이'
-        },
+          key: "user_name",
+          label: "글쓴이"
+        }
       ],
       items: items
+    };
+  },
+  computed: {
+    rows() {
+      return this.items.length;
     }
   },
   methods: {
     rowClick(item, index, e) {
       this.$router.push({
         path: `/board/free/detail/${item.content_id}`
-      })
+      });
     },
     writeContent() {
       this.$router.push({
-        path: '/board/free/create'
-      })
+        path: "/board/free/create"
+      });
     }
   }
-}
+};
 </script>
--- src/data/index.js
+++ src/data/index.js
@@ -40,7 +40,223 @@ export default {
       context: '^^',
       created_at: '2019-03-29 13:11:42',
       updated_at: null
-    }
+    },
+    {
+      content_id: 4,
+      user_id: 1,
+      title: '개린이 르라나의 강의 알람표',
+      context: '아직 미정입니다.',
+      created_at: '2019-01-01 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 5,
+      user_id: 3,
+      title: '레드벨벳 많이 사랑해 주세요^^',
+      context: '화이팅!',
+      created_at: '2019-01-02 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 6,
+      user_id: 2,
+      title: '생일 축하해주신 여러분 감사합니다!',
+      context: '^^',
+      created_at: '2019-03-29 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 7,
+      user_id: 1,
+      title: '개린이 르라나의 강의 알람표',
+      context: '아직 미정입니다.',
+      created_at: '2019-01-01 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 8,
+      user_id: 3,
+      title: '레드벨벳 많이 사랑해 주세요^^',
+      context: '화이팅!',
+      created_at: '2019-01-02 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 9,
+      user_id: 2,
+      title: '생일 축하해주신 여러분 감사합니다!',
+      context: '^^',
+      created_at: '2019-03-29 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 10,
+      user_id: 1,
+      title: '개린이 르라나의 강의 알람표',
+      context: '아직 미정입니다.',
+      created_at: '2019-01-01 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 11,
+      user_id: 3,
+      title: '레드벨벳 많이 사랑해 주세요^^',
+      context: '화이팅!',
+      created_at: '2019-01-02 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 12,
+      user_id: 2,
+      title: '생일 축하해주신 여러분 감사합니다!',
+      context: '^^',
+      created_at: '2019-03-29 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 13,
+      user_id: 1,
+      title: '개린이 르라나의 강의 알람표',
+      context: '아직 미정입니다.',
+      created_at: '2019-01-01 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 14,
+      user_id: 3,
+      title: '레드벨벳 많이 사랑해 주세요^^',
+      context: '화이팅!',
+      created_at: '2019-01-02 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 15,
+      user_id: 2,
+      title: '생일 축하해주신 여러분 감사합니다!',
+      context: '^^',
+      created_at: '2019-03-29 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 16,
+      user_id: 1,
+      title: '개린이 르라나의 강의 알람표',
+      context: '아직 미정입니다.',
+      created_at: '2019-01-01 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 17,
+      user_id: 3,
+      title: '레드벨벳 많이 사랑해 주세요^^',
+      context: '화이팅!',
+      created_at: '2019-01-02 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 18,
+      user_id: 2,
+      title: '생일 축하해주신 여러분 감사합니다!',
+      context: '^^',
+      created_at: '2019-03-29 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 19,
+      user_id: 1,
+      title: '개린이 르라나의 강의 알람표',
+      context: '아직 미정입니다.',
+      created_at: '2019-01-01 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 20,
+      user_id: 3,
+      title: '레드벨벳 많이 사랑해 주세요^^',
+      context: '화이팅!',
+      created_at: '2019-01-02 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 21,
+      user_id: 2,
+      title: '생일 축하해주신 여러분 감사합니다!',
+      context: '^^',
+      created_at: '2019-03-29 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 22,
+      user_id: 1,
+      title: '개린이 르라나의 강의 알람표',
+      context: '아직 미정입니다.',
+      created_at: '2019-01-01 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 23,
+      user_id: 3,
+      title: '레드벨벳 많이 사랑해 주세요^^',
+      context: '화이팅!',
+      created_at: '2019-01-02 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 24,
+      user_id: 2,
+      title: '생일 축하해주신 여러분 감사합니다!',
+      context: '^^',
+      created_at: '2019-03-29 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 25,
+      user_id: 1,
+      title: '개린이 르라나의 강의 알람표',
+      context: '아직 미정입니다.',
+      created_at: '2019-01-01 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 26,
+      user_id: 3,
+      title: '레드벨벳 많이 사랑해 주세요^^',
+      context: '화이팅!',
+      created_at: '2019-01-02 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 27,
+      user_id: 2,
+      title: '생일 축하해주신 여러분 감사합니다!',
+      context: '^^',
+      created_at: '2019-03-29 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 28,
+      user_id: 1,
+      title: '개린이 르라나의 강의 알람표',
+      context: '아직 미정입니다.',
+      created_at: '2019-01-01 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 29,
+      user_id: 3,
+      title: '레드벨벳 많이 사랑해 주세요^^',
+      context: '화이팅!',
+      created_at: '2019-01-02 13:11:42',
+      updated_at: null
+    },
+    {
+      content_id: 30,
+      user_id: 2,
+      title: '생일 축하해주신 여러분 감사합니다!',
+      context: '^^',
+      created_at: '2019-03-29 13:11:42',
+      updated_at: null
+    },
   ],
   Comment: [
     {
